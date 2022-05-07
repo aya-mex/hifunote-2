c9ba569ed7ba7cb293b9f3b5ab021c4ce95ed45b
aya-mex <aya.yamada03@gmail.com>
2022-05-07 11:41:42 +0900

    アクション定義

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,26 @@
 class RecordsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+
   def index
     @records = Record.all
+  end
+
+  def new
+    @record = Record.new
+  end
+
+  def create
+    @record = Record.new(record_params)
+    if @record.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:image, :onset_date, :onset_time, bodypart_ids:[], symptom_ids:[], :visit_date, :hospital_name, :diagnosis, :cause, :prescription_drug, :remission_date, :memo).merge(user_id: current_user.id)
   end
 end
