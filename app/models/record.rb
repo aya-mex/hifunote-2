5e16a496660f613dbec48e7a5f45955fea3c6a1e
aya-mex <aya.yamada03@gmail.com>
2022-05-08 12:39:23 +0900

    バリデーション修正

--- app/models/record.rb
+++ app/models/record.rb
@@ -4,6 +4,7 @@ class Record < ApplicationRecord
   has_many :comments
 
   validates :images, :onset_date, :bodypart, :symptom, presence: true
+  validates :images, length: { minimum: 1, maximum: 3 }
   validates :onset_time, presence: true,
                          numericality: { only_integer: true, message: 'is invalid. Input half-width characters' },
                          inclusion: { in: 0..23, message: 'is out of setting range' }
