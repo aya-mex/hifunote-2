5cb61a999398fa2f63535ed41dca7485052cd401
aya-mex <aya.yamada03@gmail.com>
2022-05-08 12:20:29 +0900

    アソシエーション編集

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,9 +1,9 @@
 class Record < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
+  has_many_attached :images
   has_many :comments
 
-  validates :image, :onset_date, :bodypart, :symptom, presence: true
+  validates :images, :onset_date, :bodypart, :symptom, presence: true
   validates :onset_time, presence: true,
                          numericality: { only_integer: true, message: 'is invalid. Input half-width characters' },
                          inclusion: { in: 0..23, message: 'is out of setting range' }
