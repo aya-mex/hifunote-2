e502b56d545998eb4a541e3192b401803c0592f0
aya-mex <aya.yamada03@gmail.com>
2022-05-21 21:59:13 +0900

    コントローラーでアクション定義

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,20 @@
 class FavoritesController < ApplicationController
+  before_action :set_record
+  before_action :authenticate_user!
+
+  def create
+    if @record.user_id != current_user.id
+      @favorite = Favorite.create(user_id: current_user.id, record_id: @record.id)
+    end
+  end
+  
+  def destroy
+    @favorite = Favorite.find_by(user_id: current_user.id, record_id: @record.id)
+    @favorite.destroy
+  end
+
+  private
+  def set_record
+    @record = Record.find(params[:record_id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @records = user.records.order('created_at DESC')
+    @user = User.find(params[:id])
+    @nickname = @user.nickname
+    @records = @user.records.order('created_at DESC')
+
+    favorites = Favorite.where(user_id: current_user.id).pluck(:record_id)
+    @favorite_list = Record.find(favorites) 
   end
 end
