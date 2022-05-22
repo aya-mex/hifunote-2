a855b9cc825477639b9c13e9dcf6f7d94501a6d0
aya-mex <aya.yamada03@gmail.com>
2022-05-22 20:22:27 +0900

    保存機能修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,21 +1,15 @@
 class FavoritesController < ApplicationController
-  before_action :set_record
   before_action :authenticate_user!
 
   def create
-    if @record.user_id != current_user.id
-      @favorite = Favorite.create(user_id: current_user.id, record_id: @record.id)
-    end
+    @record_favorite = Favorite.new(user_id: current_user.id, record_id: params[:record_id])
+    @record_favorite.save
+    redirect_to record_path(params[:record_id]) 
   end
   
   def destroy
-    @favorite = Favorite.find_by(user_id: current_user.id, record_id: @record.id)
-    @favorite.destroy
-  end
-
-  private
-  
-  def set_record
-    @record = Record.find(params[:record_id])
+    @record_favorite = Favorite.find_by(user_id: current_user.id, record_id: params[:record_id])
+    @record_favorite.destroy
+    redirect_to record_path(params[:record_id]) 
   end
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -17,4 +17,8 @@ class Record < ApplicationRecord
       Record.all
     end
   end
+
+  def favorited?(user)
+    favorites.where(user_id: user.id).exists?
+ end
 end
