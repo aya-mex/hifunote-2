bc144d90bf48ab14adf5fa09376ea9e8f069256b
aya-mex <aya.yamada03@gmail.com>
2022-05-23 18:24:12 +0900

    Update records_controller.rb

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,7 +3,7 @@ class RecordsController < ApplicationController
   before_action :set_record, only: [:show, :edit, :update, :destroy]
 
   def index
-    @records = Record.includes(:user).order('created_at DESC').limit(3)
+    @records = Record.includes(:user).order('created_at DESC')
   end
 
   def new
