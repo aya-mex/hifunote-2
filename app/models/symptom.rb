0eddc93bd05a33cb0c072e36ce1cc159af718571
aya-mex <aya.yamada03@gmail.com>
2022-06-24 22:30:17 +0900

    Update symptom.rb

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -9,20 +9,20 @@ class Genre < ActiveHash::Base
     { id: 7, name: 'じんましん' },
     { id: 8, name: 'アトピー' },
     { id: 9, name: 'イボ' },
-    { id: 10, name: 'タコ・ウオノメ' }
-    { id: 11, name: '腫れ' }
-    { id: 12, name: '水ぶくれ' }
-    { id: 13, name: '虫刺され' }
-    { id: 14, name: '水虫' }
-    { id: 15, name: 'やけど' }
-    { id: 16, name: '脱毛' }
-    { id: 17, name: 'しこり' }
-    { id: 18, name: 'あざ' }
-    { id: 19, name: 'ほくろ' }
+    { id: 10, name: 'タコ・ウオノメ' },
+    { id: 11, name: '腫れ' },
+    { id: 12, name: '水ぶくれ' },
+    { id: 13, name: '虫刺され' },
+    { id: 14, name: '水虫' },
+    { id: 15, name: 'やけど' },
+    { id: 16, name: '脱毛' },
+    { id: 17, name: 'しこり' },
+    { id: 18, name: 'あざ' },
+    { id: 19, name: 'ほくろ' },
     { id: 20, name: 'その他' }
   ]
 
   include ActiveHash::Associations
   has_many :records
-  
+
   end
